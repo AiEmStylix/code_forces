632566ffeb5b197653b6fb5fe88c085403ba7a5d
Vũ Bình Dương <78728283+AiEmStylix@users.noreply.github.com>
2024-09-23 01:01:23 +0700

    Solving 977A problem

---
+++ 800/977A - Wrong Subtraction.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/977/A
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    cin >> n >> k;
+    while (k--)
+    {
+        if (n % 10 == 0)
+        {
+            n /= 10;
+        }
+        else
+        {
+            n--;
+        }
+    }
+    cout << n;
+    return 0;
+}
